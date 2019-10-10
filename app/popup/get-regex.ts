e49a24ce21cff9ecc1567a4a323567ffebd16491
Gabriel Lima <email.gabriel.lima@gmail.com>
2019-10-10 18:37:58 +0200

    fix: update badge tabs
    * refactor: fix get regex
    
    * fix: update paint when switching tabs

--- app/popup/get-regex.ts
+++ app/popup/get-regex.ts
@@ -1,13 +1,7 @@
-import {CustomObjectValues, PopupData} from '../models/popup.model'
-
-export const getRegex = (data: any): RegExp | null => {
-  const dataArray: CustomObjectValues = Object.values(data.popupData)
-
-  const searchValues = dataArray
-    .filter((x: PopupData) => !!x)
-    .map((a: string) => a.split(','))
-    .map((b: []) => b.map((c: string) => c.trim()))
-    .flat()
+export const getRegex = (string: any): RegExp | null => {
+  const searchValues = string
+    .split(',')
+    .map((term: string) => term.toString())
     .join('|')
 
   return searchValues ? new RegExp(searchValues, 'gm') : null
